30a545380709b32830cf97f4adbb0e61750db86d
Berlin <tommyonik@gmail.com>
2024-05-24 17:46:44 -0400

    added correct data file - gpt changed the data when it translated to CSV into a JS file

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ container.innerHTML = rings
   .map(
     (ring) => `
         <div class="item">
-            <img src="${ring.src}" alt="${ring.Title}">
+            <img src="${ring.src}" alt="${ring.title}">
             <ul>
-              <li>Designer: ${ring.Designer}</li>
-              <li>Size: ${ring.RingSize}</li>
-              <li>Metal Type: ${ring.Metal}</li>
-              <li>Stone Type: ${ring.Stone}</li>
-              <li>Price: $${ring.SalePrice}</li>
+              <li>Designer: ${ring.designer}</li>
+              <li>Size: ${ring.ring}</li>
+              <li>Metal Type: ${ring.metal}</li>
+              <li>Stone Type: ${ring.stone}</li>
+              <li>Price: $${ring.salePrice}</li>
             </ul>
         </div>
     `
